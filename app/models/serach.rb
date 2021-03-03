148c7c14d6cca546c2abd6acc9739a2cdc6206df
yuuki-abc <yuki.abc.300@gmail.com>
2021-03-03 13:15:18 +0000

    [Finish]

--- app/models/serach.rb
+++ app/models/serach.rb
@@ -5,29 +5,23 @@ class Serach < ApplicationRecord
     if target == 'target_user'
       target = User
       column = 'name'
-      column2 = 'introduction'
     end
 
     if target == 'target_book'
       target = Book
       column = 'title'
-      column2 = 'body'
     end
 
     unless target == nil
 
       if method == "forward_match"
-        @posts = target.where("#{column} like ?","#{word}%") \
-                        or where("#{column2} like ?","%#{word}%")
+        @posts = target.where("#{column} like ?","#{word}%") # 質問 whereで二つのカラムを対象に検索する事は出来るのか？
       elsif method == "backward_match"
-        @posts = target.where("#{column} like ?","%#{word}") \
-                        or where("#{column2} like ?","%#{word}%")
+        @posts = target.where("#{column} like ?","%#{word}")
       elsif method == "perfect_match"
-        @posts = target.where("#{column} like ?","#{word}") \
-                        or where("#{column2} like ?","%#{word}%")
+        @posts = target.where("#{column} like ?","#{word}")
       elsif method == "partial_match"
-        @posts = target.where("#{column} like ?","%#{word}%") \
-                        or where("#{column2} like ?","%#{word}%")
+        @posts = target.where("#{column} like ?","%#{word}%")
       else
         @posts = target.all
       end
